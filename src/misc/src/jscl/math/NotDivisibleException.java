e7633fe7955d3e51bc76bad634e074414cb3787e
Sergey Solovyev <serso1988@gmail.com>
2011-09-16 17:06:11 +0400

    sources removed

--- src/misc/src/jscl/math/NotDivisibleException.java
+++
@@ -1,9 +0,0 @@
-package jscl.math;
-
-public class NotDivisibleException extends ArithmeticException {
-        public NotDivisibleException() {}
-
-        public NotDivisibleException(String s) {
-                super(s);
-        }
-}
--- src/misc/src/jscl/math/NotVariableException.java
+++
@@ -1,9 +0,0 @@
-package jscl.math;
-
-public class NotVariableException extends ArithmeticException {
-        public NotVariableException() {}
-
-        public NotVariableException(String s) {
-                super(s);
-        }
-}
