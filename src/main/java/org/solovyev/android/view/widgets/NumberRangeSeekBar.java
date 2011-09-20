1e36fc56aa3c74d70b89323bc249451bca4b5338
Sergey Solovyev <serso1988@gmail.com>
2011-09-20 18:20:39 +0400

    some changes

--- src/main/java/org/solovyev/android/view/widgets/NumberRangeSeekBar.java
+++ src/main/java/org/solovyev/android/view/widgets/NumberRangeSeekBar.java
@@ -2,6 +2,7 @@ package org.solovyev.android.view.widgets;
 
 import android.content.Context;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.solovyev.common.utils.Converter;
 import org.solovyev.common.utils.NumberValuer;
 
@@ -20,11 +21,12 @@ public class NumberRangeSeekBar<T extends Number> extends AbstractRangeSeekBar<T
 	 *
 	 * @param minValue The minimum value of the selectable range.
 	 * @param maxValue The maximum value of the selectable range.
+	 * @param steps number of steps of range
 	 * @param context  parent context
 	 * @throws IllegalArgumentException Will be thrown if min/max value types are not one of Long, Double, Integer, Float, Short, Byte or BigDecimal.
 	 */
-	public NumberRangeSeekBar(@NotNull T minValue, @NotNull T maxValue, Context context) throws IllegalArgumentException {
-		super(minValue, maxValue, context);
+	public NumberRangeSeekBar(@NotNull T minValue, @NotNull T maxValue, @Nullable Integer steps, Context context) throws IllegalArgumentException {
+		super(minValue, maxValue, steps, context);
 
 		numberType = NumberType.fromNumber(minValue);
 
@@ -36,8 +38,8 @@ public class NumberRangeSeekBar<T extends Number> extends AbstractRangeSeekBar<T
 		return new Converter<Double, T>() {
 			@NotNull
 			@Override
-			public T convert(@NotNull Double aDouble) {
-				return (T) numberType.toNumber(aDouble);
+			public T convert(@NotNull Double value) {
+				return (T) numberType.toNumber(value);
 			}
 		};
 	}
