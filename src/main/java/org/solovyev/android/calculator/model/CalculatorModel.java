800156a17f715cc38965a178d3ea18c75d0da817
serso <se.solovyev@gmail.com>
2011-10-13 13:28:06 +0400

    CE

--- src/main/java/org/solovyev/android/calculator/model/CalculatorModel.java
+++ src/main/java/org/solovyev/android/calculator/model/CalculatorModel.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.solovyev.android.calculator.JsclOperation;
 import org.solovyev.common.NumberMapper;
+import org.solovyev.common.utils.MutableObject;
 
 /**
  * User: serso
