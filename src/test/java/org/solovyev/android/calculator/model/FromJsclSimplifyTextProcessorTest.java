b3591242e666e9b49ac516b8010e086529d960ec
serso <se.solovyev@gmail.com>
2011-10-22 17:59:40 +0400

    symbolic calculations

--- src/test/java/org/solovyev/android/calculator/model/FromJsclSimplifyTextProcessorTest.java
+++ src/test/java/org/solovyev/android/calculator/model/FromJsclSimplifyTextProcessorTest.java
@@ -1,10 +1,8 @@
 package org.solovyev.android.calculator.model;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.solovyev.android.calculator.jscl.JsclOperation;
 
 /**
  * User: serso
@@ -21,12 +19,36 @@ public class FromJsclSimplifyTextProcessorTest {
 	@Test
 	public void testProcess() throws Exception {
 		FromJsclSimplifyTextProcessor tp = new FromJsclSimplifyTextProcessor();
-		Assert.assertEquals("t11×e", tp.process("t11*2.718281828459045"));
+		Assert.assertEquals("t11e", tp.process("t11*2.718281828459045"));
 		Assert.assertEquals("e", tp.process("2.718281828459045"));
-		Assert.assertEquals("te×e", tp.process("t2.718281828459045*2.718281828459045"));
+		Assert.assertEquals("tee", tp.process("t2.718281828459045*2.718281828459045"));
 
 		CalculatorEngine.instance.getVarsRegister().addVar(null, new Var.Builder("t2.718281828459045", "2"));
-		Assert.assertEquals("t2.718281828459045×e", tp.process("t2.718281828459045*2.718281828459045"));
+		Assert.assertEquals("t2.718281828459045e", tp.process("t2.718281828459045*2.718281828459045"));
+		Assert.assertEquals("t×", tp.process("t*"));
+		Assert.assertEquals("×t", tp.process("*t"));
+		Assert.assertEquals("t×2", tp.process("t*2"));
+		Assert.assertEquals("2×t", tp.process("2*t"));
+		CalculatorEngine.instance.getVarsRegister().addVar(null, new Var.Builder("t", (String)null));
+		Assert.assertEquals("t×", tp.process("t*"));
+		Assert.assertEquals("×t", tp.process("*t"));
+
+		Assert.assertEquals("t2", tp.process("t*2"));
+		Assert.assertEquals("2t", tp.process("2*t"));
+
+		Assert.assertEquals("t^2×2", tp.process("t^2*2"));
+		Assert.assertEquals("2t^2", tp.process("2*t^2"));
+
+		Assert.assertEquals("t^[2×2t]", tp.process("t^[2*2*t]"));
+		Assert.assertEquals("2t^2[2t]", tp.process("2*t^2[2*t]"));
+
+		CalculatorEngine.instance.getVarsRegister().addVar(null, new Var.Builder("k", (String)null));
+		Assert.assertEquals("(t+2k)[k+2t]", tp.process("(t+2*k)*[k+2*t]"));
+		Assert.assertEquals("(te+2k)e[k+2te]", tp.process("(t*e+2*k)*e*[k+2*t*e]"));
+
+
+		Assert.assertEquals("tln(3)", tp.process("t*log(3)"));
+		Assert.assertEquals("t√(3)", tp.process("t*sqrt(3)"));
 
 	}
 }
