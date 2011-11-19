e7e7d7aca508d90b40e4b8a7f1fd57fe27dc9cca
serso <serso@serso-laptop.(none)>
2011-11-19 16:06:24 +0400

    android_calculator-5: App help

---
+++ src/main/java/org/solovyev/android/calculator/help/HelpHintsActivity.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2009-2011. Created by serso aka se.solovyev.
+ * For more information, please, contact se.solovyev@gmail.com
+ * or visit http://se.solovyev.org
+ */
+
+package org.solovyev.android.calculator.help;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.jetbrains.annotations.Nullable;
+import org.solovyev.android.calculator.R;
+
+/**
+ * User: serso
+ * Date: 11/19/11
+ * Time: 11:37 AM
+ */
+public class HelpHintsActivity extends Activity {
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.help_hints);
+	}
+}
