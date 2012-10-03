c7203068916c5500bd4c3f531c547193567aa3fd
serso <se.solovyev@gmail.com>
2012-10-03 16:43:06 +0400

    Changes

--- calculatorpp/src/main/java/org/solovyev/android/calculator/about/CalculatorAboutActivity.java
+++ calculatorpp/src/main/java/org/solovyev/android/calculator/about/CalculatorAboutActivity.java
@@ -1,31 +1,27 @@
 /*
  * Copyright (c) 2009-2011. Created by serso aka se.solovyev.
  * For more information, please, contact se.solovyev@gmail.com
- * or visit http://se.solovyev.org
  */
 
 package org.solovyev.android.calculator.about;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
-import android.widget.TextView;
 import org.jetbrains.annotations.Nullable;
+import org.solovyev.android.calculator.CalculatorFragmentActivity;
 import org.solovyev.android.calculator.R;
 
 /**
  * User: serso
- * Date: 12/24/11
- * Time: 11:55 PM
+ * Date: 9/16/11
+ * Time: 11:52 PM
  */
-public class CalculatorAboutActivity extends Activity {
+public class CalculatorAboutActivity extends CalculatorFragmentActivity {
 
-	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		final TextView about = (TextView) findViewById(R.id.aboutTextView);
-		about.setMovementMethod(LinkMovementMethod.getInstance());
-	}
+        getActivityHelper().addTab(this, CalculatorFragmentType.about, null, R.id.main_layout);
+        getActivityHelper().addTab(this, CalculatorFragmentType.release_notes, null, R.id.main_layout);
+    }
 }
