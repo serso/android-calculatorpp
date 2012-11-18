029137f2c9a5a8b1f455a9ff96c3a57b546cf288
Sergey Solovyev <se.solovyev@gmail.com>
2012-11-18 20:47:55 +0400

    Fixes

---
+++ calculatorpp/src/main/java/org/solovyev/android/calculator/widget/CalculatorWidgetProvider4x5.java
@@ -0,0 +1,9 @@
+package org.solovyev.android.calculator.widget;
+
+/**
+ * User: serso
+ * Date: 11/18/12
+ * Time: 7:43 PM
+ */
+public class CalculatorWidgetProvider4x5 extends AbstractCalculatorWidgetProvider {
+}
