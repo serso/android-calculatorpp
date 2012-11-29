3d167021a67e99203721654ad8f17271ba7ff9e6
Sergey Solovyev <se.solovyev@gmail.com>
2012-11-29 20:47:08 +0400

    monkey script

--- calculatorpp/misc/other/take_screenshots2.sh
+++ calculatorpp/misc/other/take_screenshots2.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# "AVD_4.1_x86_6" "AVD_4.0.3_x86" "AVD_4.1_x86" "Default" "AVD_4.0.3_x86_7" "AVD_4.1_x86_7" "AVD_4.1_x86_9.5" "Galaxy_Tab" "AVD_4.1_x86_4"
-# "AVD_4.1_x86_6" "AVD_4.1_x86_7" "AVD_4.1_x86_9.5" "AVD_4.1_x86_4"
+predefined=1
+scale=0.51
 
-declare -a names=("AVD_4.1_x86_4")
+# first predefined
 
-for name in ${names[@]}
-do
-    $ANDROID_HOME/tools/emulator -avd $name &
-    $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ~/projects/java/android/calculatorpp/calculatorpp/misc/aux/tmp $name
-    $ANDROID_HOME/platform-tools/adb -s emulator-5580 emu kill
-done
+if [ $predefined -eq 1 ]
+then
+    declare -a names=("AVD_Nexus_7_by_Google")
+    for name in ${names[@]}
+    do
+                $ANDROID_HOME/tools/emulator -ports 5580,5581 -avd $name -scale $scale &
+                sleep 50
+                $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ~/projects/java/android/calculatorpp/calculatorpp/misc/other/tmp/2012.11.25 $name
+                $ANDROID_HOME/platform-tools/adb -s emulator-5580 emu kill
+                sleep 3
+    done
+fi
