b9a90f79f9d86f400eaa1dc3fa125a70c24135e1
Sergey Solovyev <se.solovyev@gmail.com>
2012-11-30 18:14:00 +0400

    removed gen folder

--- android-app/gen-external-apklibs/org.solovyev.android_android-common-all_1.0.6/gen/org/solovyev/android/all/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.solovyev.android.all;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android-app/gen-external-apklibs/org.solovyev.android_android-common-sherlock_1.0.6/gen/org/solovyev/android/sherlock/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.solovyev.android.sherlock;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
