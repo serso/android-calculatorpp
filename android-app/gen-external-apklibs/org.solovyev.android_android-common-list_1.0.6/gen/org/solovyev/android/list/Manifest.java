fb53604b3fca94f1feb75cbfe5c4283573c978f0
Sergey Solovyev <se.solovyev@gmail.com>
2012-11-30 18:21:16 +0400

    removed gen folder

--- android-app/gen-external-apklibs/org.solovyev.android_android-common-list_1.0.6/gen/org/solovyev/android/list/Manifest.java
+++
@@ -1,5 +0,0 @@
-package org.solovyev.android.list;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
--- android-app/gen-external-apklibs/org.solovyev.android_android-common-list_1.0.6/gen/org/solovyev/android/list/R.java
+++
@@ -1,5 +0,0 @@
-package org.solovyev.android.list;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
