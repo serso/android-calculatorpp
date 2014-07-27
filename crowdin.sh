76fafedd81e5ee32ca78e06add3c6a349de8fac9
serso <se.solovyev@gmail.com>
2014-07-27 18:35:42 +0200

    new languages support

--- crowdin.sh
+++ crowdin.sh
@@ -15,17 +15,23 @@ function copyTranslation {
     cp $from $to
 }
 
+copyTranslation "translations/ar/app-android/*" "android-app-core/res/values-ar"
+copyTranslation "translations/cs/app-android/*" "android-app-core/res/values-cs"
 copyTranslation "translations/en/app-android/*" "android-app-core/res/values"
 copyTranslation "translations/es-ES/app-android/*" "android-app-core/res/values-es"
 copyTranslation "translations/de/app-android/*" "android-app-core/res/values-de"
+copyTranslation "translations/fi/app-android/*" "android-app-core/res/values-fi"
 copyTranslation "translations/fr/app-android/*" "android-app-core/res/values-fr"
 copyTranslation "translations/it/app-android/*" "android-app-core/res/values-it"
 copyTranslation "translations/nl/app-android/*" "android-app-core/res/values-nl"
 copyTranslation "translations/pl/app-android/*" "android-app-core/res/values-pl"
 copyTranslation "translations/pt-BR/app-android/*" "android-app-core/res/values-pt-rbr"
 copyTranslation "translations/ru/app-android/*" "android-app-core/res/values-ru"
+copyTranslation "translations/vi/app-android/*" "android-app-core/res/values-vi"
 copyTranslation "translations/uk/app-android/*" "android-app-core/res/values-uk"
-copyTranslation "translations/zh-CN/app-android/*" "android-app-core/res/values-zh-rtw"
+copyTranslation "translations/ja/app-android/*" "android-app-core/res/values-ja"
+copyTranslation "translations/zh-CN/app-android/*" "android-app-core/res/values-zh-rcn"
+copyTranslation "translations/zh-TW/app-android/*" "android-app-core/res/values-zh-rtw"
 
 
 rm -r translations
