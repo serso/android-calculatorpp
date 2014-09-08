35eeb72de35381990cccf1b4ae6e606c2dc836ba
serso <se.solovyev@gmail.com>
2014-09-08 18:40:57 +0200

    new translations

--- crowdin.sh
+++ crowdin.sh
@@ -3,35 +3,61 @@
 rm -r translations
 unzip calculatorpp.zip -d translations
 
-function copyTranslation {
+function copyTranslation() {
     from=$1
     to=$2
 
-	if [ ! -d $to ]; then
-    	# if directory doesn't exist create it
-    	mkdir $to
-    fi
-
-    cp $from $to
+    cp ${from} ${to}
 }
 
-copyTranslation "translations/ar/app-android/*" "android-app-core/res/values-ar"
-copyTranslation "translations/cs/app-android/*" "android-app-core/res/values-cs"
-copyTranslation "translations/en/app-android/*" "android-app-core/res/values"
-copyTranslation "translations/es-ES/app-android/*" "android-app-core/res/values-es"
-copyTranslation "translations/de/app-android/*" "android-app-core/res/values-de"
-copyTranslation "translations/fi/app-android/*" "android-app-core/res/values-fi"
-copyTranslation "translations/fr/app-android/*" "android-app-core/res/values-fr"
-copyTranslation "translations/it/app-android/*" "android-app-core/res/values-it"
-copyTranslation "translations/nl/app-android/*" "android-app-core/res/values-nl"
-copyTranslation "translations/pl/app-android/*" "android-app-core/res/values-pl"
-copyTranslation "translations/pt-BR/app-android/*" "android-app-core/res/values-pt-rbr"
-copyTranslation "translations/ru/app-android/*" "android-app-core/res/values-ru"
-copyTranslation "translations/vi/app-android/*" "android-app-core/res/values-vi"
-copyTranslation "translations/uk/app-android/*" "android-app-core/res/values-uk"
-copyTranslation "translations/ja/app-android/*" "android-app-core/res/values-ja"
-copyTranslation "translations/zh-CN/app-android/*" "android-app-core/res/values-zh-rcn"
-copyTranslation "translations/zh-TW/app-android/*" "android-app-core/res/values-zh-rtw"
+function copyTranslations() {
+    from="translations/$1/app-android/*"
+    to="android-app-core/res/values-$2"
+    if [ -z "${2}" ]; then
+        to="android-app-core/res/values"
+	fi
+
+	if [ ! -d ${to} ]; then
+    	# if directory doesn't exist create it
+    	mkdir ${to}
+    fi
+
+	copyTranslation "${from}" "${to}"
+
+	from="translations/$1/app/messages.properties"
+    to="core/src/main/resources/org/solovyev/android/calculator/messages_${1//[-]/_}.properties"
+    if [ -z "${2}" ]; then
+        to="core/src/main/resources/org/solovyev/android/calculator/messages.properties"
+	fi
+
+	copyTranslation "${from}" "${to}"
+
+	from="translations/$1/jscl/messages.properties"
+    to="../../jscl/src/main/resources/jscl/text/msg/messages_${1//[-]/_}.properties"
+    if [ -z "${2}" ]; then
+        to="../../jscl/src/main/resources/jscl/text/msg/messages.properties"
+	fi
+
+	copyTranslation "${from}" "${to}"
+}
+
+copyTranslations "ar" "ar"
+copyTranslations "cs" "cs"
+copyTranslations "en" ""
+copyTranslations "es-ES" "es"
+copyTranslations "de" "de"
+copyTranslations "fi" "fi"
+copyTranslations "fr" "fr"
+copyTranslations "it" "it"
+copyTranslations "nl" "nl"
+copyTranslations "pl" "pl"
+copyTranslations "pt-BR" "pt-rbr"
+copyTranslations "ru" "ru"
+copyTranslations "vi" "vi"
+copyTranslations "uk" "uk"
+copyTranslations "ja" "ja"
+copyTranslations "zh-CN" "zh-rcn"
+copyTranslations "zh-TW" "zh-rtw"
 
 
 rm -r translations
