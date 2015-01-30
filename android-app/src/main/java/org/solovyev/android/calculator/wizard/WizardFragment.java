5a758e7acd8c2167310d7374a03aee9216e7264c
serso <se.solovyev@gmail.com>
2015-01-30 22:15:37 +0100

    Wizard styling

--- android-app/src/main/java/org/solovyev/android/calculator/wizard/WizardFragment.java
+++ android-app/src/main/java/org/solovyev/android/calculator/wizard/WizardFragment.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import org.solovyev.android.calculator.R;
+import org.solovyev.android.wizard.WizardFlow;
+import org.solovyev.android.wizard.WizardStep;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public abstract class WizardFragment extends Fragment implements View.OnClickListener {
@@ -18,6 +21,26 @@ public abstract class WizardFragment extends Fragment implements View.OnClickLis
 
 	@Nullable
 	protected TextView prevButton;
+
+	private WizardStep step;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		step = findStepByClassName();
+	}
+
+	@Nonnull
+	private CalculatorWizardStep findStepByClassName() {
+		for (CalculatorWizardStep step : CalculatorWizardStep.values()) {
+			if(step.getFragmentClass().equals(getClass())) {
+				return step;
+			}
+		}
+
+		throw new AssertionError("Wizard step for class " + getClass() + " was not found");
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,6 +56,25 @@ public abstract class WizardFragment extends Fragment implements View.OnClickLis
 		prevButton = (TextView) view.findViewById(R.id.wizard_prev);
 		if (prevButton != null) {
 			prevButton.setOnClickListener(this);
+		}
+
+		final WizardFlow flow = getWizardActivity().getFlow();
+		final boolean canGoNext = flow.getNextStep(step) != null;
+		final boolean canGoPrev = flow.getPrevStep(step) != null;
+		if (canGoNext) {
+			if (canGoPrev) {
+				setupNextButton(R.string.acl_wizard_next);
+			} else {
+				setupNextButton(R.string.acl_wizard_start);
+			}
+		} else {
+			setupNextButton(R.string.acl_wizard_finish);
+		}
+
+		if (canGoPrev) {
+			setupPrevButton(R.string.acl_wizard_back);
+		} else {
+			setupPrevButton(R.string.wizard_skip);
 		}
 
 		return view;
