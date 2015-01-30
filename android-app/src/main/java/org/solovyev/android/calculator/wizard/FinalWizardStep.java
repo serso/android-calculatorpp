20b408dc5c9c6f97449463682f092503ae4488cb
serso <se.solovyev@gmail.com>
2015-01-30 23:02:44 +0100

    Final wizard step

--- android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
+++ android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
@@ -1,59 +1,11 @@
 package org.solovyev.android.calculator.wizard;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import org.solovyev.android.calculator.R;
-import org.solovyev.android.calculator.preferences.PurchaseDialogActivity;
-
-import static android.content.Intent.ACTION_VIEW;
 
 public class FinalWizardStep extends WizardFragment {
-
-	private static final String GITHUB_URL = "https://github.com/serso/android-calculatorpp";
-	private static final String CROWDIN_URL = "http://crowdin.net/project/calculatorpp";
-
-	private Button donateButton;
-	private Button translateButton;
-	private Button contributeButton;
 
 	@Override
 	protected int getViewResId() {
 		return R.layout.cpp_wizard_step_final;
-	}
-
-	@Override
-	public void onViewCreated(View root, Bundle savedInstanceState) {
-		super.onViewCreated(root, savedInstanceState);
-
-		donateButton = (Button) root.findViewById(R.id.cpp_wizard_final_donate_button);
-		donateButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getActivity(), PurchaseDialogActivity.class));
-			}
-		});
-
-		translateButton = (Button) root.findViewById(R.id.cpp_wizard_final_translate_button);
-		translateButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				showUrl(CROWDIN_URL);
-			}
-		});
-
-		contributeButton = (Button) root.findViewById(R.id.cpp_wizard_final_contribute_button);
-		contributeButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				showUrl(GITHUB_URL);
-			}
-		});
-	}
-
-	private void showUrl(String url) {
-		startActivity(new Intent(ACTION_VIEW, Uri.parse(url)));
 	}
 }
