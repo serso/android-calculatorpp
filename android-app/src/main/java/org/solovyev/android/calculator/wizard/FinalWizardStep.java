a8e20a14a3a70ddf87ad63fa58d09f5b99c4ee3e
serso <se.solovyev@gmail.com>
2015-02-09 23:26:59 +0100

    Blue Done icon for light wizard

--- android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
+++ android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
@@ -1,5 +1,9 @@
 package org.solovyev.android.calculator.wizard;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import org.solovyev.android.calculator.App;
 import org.solovyev.android.calculator.R;
 
 public class FinalWizardStep extends WizardFragment {
@@ -7,5 +11,14 @@ public class FinalWizardStep extends WizardFragment {
 	@Override
 	protected int getViewResId() {
 		return R.layout.cpp_wizard_step_final;
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		if (App.getTheme().isLight()) {
+			final TextView message = (TextView) view.findViewById(R.id.wizard_final_message);
+			message.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_action_done_blue, 0, 0);
+		}
 	}
 }
