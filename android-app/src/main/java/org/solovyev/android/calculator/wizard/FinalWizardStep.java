3d1731995a68533853a9de9a2296db0a55387956
serso <se.solovyev@gmail.com>
2015-02-10 11:28:45 +0100

    Light color correction

--- android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
+++ android-app/src/main/java/org/solovyev/android/calculator/wizard/FinalWizardStep.java
@@ -2,6 +2,7 @@ package org.solovyev.android.calculator.wizard;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import org.solovyev.android.calculator.App;
 import org.solovyev.android.calculator.R;
 
@@ -16,8 +17,8 @@ public class FinalWizardStep extends WizardFragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		if (App.getTheme().isLight()) {
-			/*final TextView message = (TextView) view.findViewById(R.id.wizard_final_message);
-			message.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_action_done_blue, 0, 0);*/
+			final TextView message = (TextView) view.findViewById(R.id.wizard_final_message);
+			message.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_action_done_light, 0, 0);
 		}
 	}
 }
