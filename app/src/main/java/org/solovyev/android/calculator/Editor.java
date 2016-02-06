f88f57368dd1abed1db48d9e56830a9ec733deb5
serso <se.solovyev@gmail.com>
2016-02-06 21:50:41 +0100

    Add Hugo library

--- app/src/main/java/org/solovyev/android/calculator/Editor.java
+++ app/src/main/java/org/solovyev/android/calculator/Editor.java
@@ -27,6 +27,7 @@ import android.content.SharedPreferences;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
+import hugo.weaving.DebugLog;
 import org.solovyev.android.Check;
 import org.solovyev.android.calculator.text.TextProcessorEditorResult;
 import org.solovyev.android.calculator.view.EditorTextProcessor;
@@ -55,6 +56,7 @@ public class Editor {
         textProcessor = new EditorTextProcessor(preferences);
     }
 
+    @DebugLog
     public void init() {
         bus.register(this);
     }
